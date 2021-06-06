57112ea8df721b36711b0d887b419a248b823c1b
aabdelw1 <ammar.s.nasr@gmail.com>
2021-06-06 11:47:01 -0500

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,8 @@ module.exports = {
   siteMetadata: {
     title: "Ammar's Website",
   },
-  plugins: ["gatsby-plugin-styled-components"],
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-emotion"
+  ],
 };
